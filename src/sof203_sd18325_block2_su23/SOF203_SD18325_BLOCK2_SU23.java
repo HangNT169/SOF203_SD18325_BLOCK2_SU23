cc1a0676afbcfb72d0af0e6eb5feeb03cb976a09
HangNT169 <HangNT169@fpt.edu.vn>
2023-06-03 17:43:23 +0700

    Init project

---
+++ src/sof203_sd18325_block2_su23/SOF203_SD18325_BLOCK2_SU23.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sof203_sd18325_block2_su23;
+
+/**
+ *
+ * @author hangnt
+ */
+public class SOF203_SD18325_BLOCK2_SU23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
